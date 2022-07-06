d8936576e1fa55a5cdee89b6ef659e583d59c5b2
FinnRG <finngaertner@protonmail.com>
2022-07-06 18:28:41 +0200

    fix(auth): Use nullish coalescing instead of or operator

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -22,7 +22,7 @@ interface RedirectState {
 
 const SignInScreen: React.FC = () => {
   const [isSignedIn, setIsSignedIn] = useState(false) // Local signed-in state.
-  const { from = '/profile/me' }: RedirectState = ((useLocation().state as RedirectState | null) != null) || { from: '/profile/me' }
+  const { from = '/profile/me' }: RedirectState = (useLocation().state as RedirectState | null) ?? { from: '/profile/me' }
 
   // Listen to the Firebase Auth state and set the local state.
   useEffect(() => {
