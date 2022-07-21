86e8d78702dc397eab7b38ae4cfabd1b5402a94e
FinnRG <finngaertner@protonmail.com>
2022-07-21 17:02:05 +0200

    feat(i18n): Setup basic translation

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
 import { render } from 'preact'
 
+import i18n from "i18next"
+import { initReactI18next  } from 'react-i18next'
+import translation from "../translation/app.json"
+
 import App from './components/app'
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: translation,
+        lng: "en",
+        interpolation: {
+            escapeValue: false
+        }
+    })
 
 render(<App />, document.getElementById('app') as HTMLElement)
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,10 +1,15 @@
+import { useTranslation } from 'react-i18next'
 import { Outlet } from 'react-router'
 
-const Home = (): JSX.Element => (
-  <div>
+const Home = (): JSX.Element => {
+
+  const {t, i18n} = useTranslation()
+
+  return <div>
     <p>Home</p>
+    <p>{t('Welcome to React')}</p>
     <Outlet />
   </div>
-)
+}
 
 export default Home
